54f5928d009d3b5781efdfe8d8d9759f07dd634d
udyanojha <use 51847297+udyanojha@users.noreply.github.com>
2022-08-11 05:43:47 +0530

    Started making TicTacToe

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,73 @@
-package PACKAGE_NAME;public class TicTacToe {
+import java.util.Scanner;
+
+public class TicTacToe {
+    Scanner s = new Scanner(System.in);
+    String[] players = new String[2];
+    char[][] board = new char[3][3];
+
+    void inputPlayerNames() {
+        System.out.println("Enter Player 1 name : ");
+        this.players[0] = s.next();
+        System.out.println("Enter Player 2 name : ");
+        this.players[1] = s.next();
+    }
+
+    boolean checkValidIfWinner(int row, int col, int currentPLayer, int turns) {
+        if(row<0||row>3||col<0||col>3||board[row][col]!=0) {
+            System.out.println("Please enter in valid range: ");
+            return false;
+        }
+        char xOrO = (currentPLayer==1)?'X':'O';
+        board[row][col] = xOrO;
+        if(turns>=5) {
+            // TODO check winner here
+            // horizontal
+            boolean winner = false;
+            for(int j = 0; j<3; j++) {
+                if(board[row][j]!=xOrO) {
+                    break;
+                }
+                winner = true;
+                System.out.println();
+            }
+        }
+        return true;
+    }
+
+    void printBoard() {
+        System.out.println("__________");
+        System.out.println("|"+board[0][0]+"| |"+board[0][1]+"| |"+board[0][2]+"|");
+        System.out.println("|"+board[1][0]+"| |"+board[1][1]+"| |"+board[1][2]+"|");
+        System.out.println("|"+board[2][0]+"| |"+board[2][1]+"| |"+board[2][2]+"|");
+        System.out.println("__________");
+    }
+
+    void play() {
+        inputPlayerNames();
+//        boolean result = false;
+        int currentPlayer = 1;
+        int turns = 1;
+        while(turns<=9) {
+            String currentPlayerName;
+            if(currentPlayer==1) {
+                currentPlayerName = players[0];
+            } else {
+                currentPlayerName = players[1];
+            }
+            System.out.println("Enter cell no "+currentPlayerName+" ");
+            int row = s.nextInt();
+            int col = s.nextInt();
+
+            while(!checkValidIfWinner(row, col, currentPlayer, turns)) {
+                row = s.nextInt();
+                col = s.nextInt();
+            }
+            // TODO Print board
+            printBoard();
+            turns++;
+            currentPlayer = currentPlayer%2+1;
+
+        }
+    }
+
 }
--- src/TicTacToePlayer.java
+++ src/TicTacToePlayer.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class TicTacToePlayer {
+public class TicTacToePlayer {
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+//        game.inputPlayerNames();
+//        System.out.println(game.player1);
+//        System.out.println(game.player2);
+        game.play();
+    }
 }
